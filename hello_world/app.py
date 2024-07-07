177f8c253c75daa504e56f6017dba8c02d28d560
Shashank Sharma <shashank9487@gmail.com>
2024-07-07 13:53:32 -0400

    added event

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,8 +4,9 @@ import boto3
 
 def lambda_handler(event, context):
     pipeline = boto3.client('codepipeline')
+    print('event', event)
+    print('context', context)
     jobId=event['CodePipeline.job']['id']
-    url = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
     response = pipeline.put_job_success_result(
         jobId=jobId
     )
