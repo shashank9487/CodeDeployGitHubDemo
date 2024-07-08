30d282abfc8b769f24efc07c210e16fd1f411fd0
Shashank Sharma <shashank9487@gmail.com>
2024-07-07 22:18:46 -0400

    Added chip data example

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,20 @@
 import pandas as pd
+from decimal import Decimal
+
+
+def create_price_label(row):
+    price = Decimal(row['price'])
+    price = round(price, 2)
+    return row['name'].title() + " $" + str(price)
 
 def lambda_handler(event, context):
-    data = {'col1': [1, 2], 'col2': [3,4], 'col3': None}
-    df = pd.DataFrame(data=data)
-    df = df.fillna(0)
-    print(df)
-    print("Done pd test")
+    url = 'https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv'
+    chipo = pd.read_csv(url, sep = '\t')
+    prices = [float(value[1 : -1]) for value in chipo.item_price]
+    chipo.item_price = prices
+    chipo_filtered = chipo.drop_duplicates(['item_name','quantity','choice_description'])
+    chipo_one_prod = chipo_filtered[chipo_filtered.quantity == 1]
+    print(chipo_one_prod)
+    chipo.item_name.sort_values()
+    print("Finished")
+    
