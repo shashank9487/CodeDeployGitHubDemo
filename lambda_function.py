94b860a0e7feed3480a65c27f6a208dc605c1c40
Shashank Sharma <shashank9487@gmail.com>
2024-07-07 22:01:04 -0400

    added fillna

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    data = {'col1': [1, 2], 'col2': [3,4]}
+    data = {'col1': [1, 2], 'col2': [3,4], 'col3': None}
     df = pd.DataFrame(data=data)
+    df = df.fillna(0)
     print(df)
     print("Done test")
