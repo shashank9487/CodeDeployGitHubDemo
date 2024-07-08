3dfcff17e4414a94999a5faf8990f24ea0a0078d
Shashank Sharma <shashank9487@gmail.com>
2024-07-08 18:52:44 -0400

    added specs

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,13 +1,20 @@
-import json
-import boto3
+import pandas as pd
+from decimal import Decimal
 
+
+def create_price_label(row):
+    price = Decimal(row['price'])
+    price = round(price, 2)
+    return row['name'].title() + " $" + str(price)
 
 def lambda_handler(event, context):
-    pipeline = boto3.client('codepipeline')
-    print('event', event)
-    print('context', context)
-    jobId=event['CodePipeline.job']['id']
-    response = pipeline.put_job_success_result(
-        jobId=jobId
-    )
-    return response
+    url = 'https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv'
+    chipo = pd.read_csv(url, sep = '\t')
+    prices = [float(value[1 : -1]) for value in chipo.item_price]
+    chipo.item_price = prices
+    chipo_filtered = chipo.drop_duplicates(['item_name','quantity','choice_description'])
+    chipo_one_prod = chipo_filtered[chipo_filtered.quantity == 1]
+    chipo_one_prod.fillna(0, inplace=True)
+    print(chipo_one_prod)
+    chipo.item_name.sort_values()
+    print("Finished")
