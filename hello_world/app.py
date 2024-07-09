72097df40644ca3f98acdaee0e288366cbdccd77
Shashank Sharma <shashank9487@gmail.com>
2024-07-08 21:19:06 -0400

    added success and failure response

--- hello_world/app.py
+++ hello_world/app.py
@@ -10,21 +10,30 @@ def create_price_label(row):
     return row['name'].title() + " $" + str(price)
 
 def lambda_handler(event, context):
-    client = boto3.client('codepipeline')
-    print('event', event)
-    url = 'https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv'
-    chipo = pd.read_csv(url, sep = '\t')
-    prices = [float(value[1 : -1]) for value in chipo.item_price]
-    chipo.item_price = prices
-    chipo_filtered = chipo.drop_duplicates(['item_name','quantity','choice_description'])
-    chipo_one_prod = chipo_filtered[chipo_filtered.quantity == 1]
-    chipo_one_prod.fillna(0, inplace=True)
-    print(chipo_one_prod)
-    chipo.item_name.sort_values()
-    print("Finished tab")
-    
-    jobId = event['CodePipeline.job']['id']
-    response = client.put_job_success_result(
-        jobId=jobId
-    )
+    try:
+        client = boto3.client('codepipeline')
+        print('event', event)
+        url = 'https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv'
+        chipo = pd.read_csv(url, sep = '\t')
+        prices = [float(value[1 : -1]) for value in chipo.item_price]
+        chipo.item_price = prices
+        chipo_filtered = chipo.drop_duplicates(['item_name','quantity','choice_description'])
+        chipo_one_prod = chipo_filtered[chipo_filtered.quantity == 1]
+        chipo_one_prod.fillna(0, inplace=True)
+        print(chipo_one_prod)
+        chipo.item_name.sort_values()
+        print("Finished tab")
+        
+        jobId = event['CodePipeline.job']['id']
+        response = client.put_job_success_result(
+            jobId=jobId
+        )
+    except Exception as e:
+        response = client.put_job_failure_result(
+            jobId=jobId,
+             failureDetails={
+                'type': 'JobFailed',
+                'message': e
+            }       
+        )
     return response
